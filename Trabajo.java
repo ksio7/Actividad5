36ea9c847a325499e37b95be8f0218753aba61ee
rdelromero <124194247+rdelromero@users.noreply.github.com>
2023-02-01 19:08:40 +0100

    Add files via upload

---
+++ Trabajo.java
@@ -0,0 +1,21 @@
+
+public class Trabajo {
+	//Atributos de instancia
+	private String idTrabajo, descripcion; private double minSalario, maxSalario;
+
+	//Métodos constructores	
+	public Trabajo(String idTrabajo, String descripcion, double minSala, double maxSalario) {
+		this.idTrabajo = idTrabajo; this.descripcion = descripcion; this.minSalario = minSala; this.maxSalario = maxSalario;
+	}
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	//Método toString por defecto;
+	@Override
+	public String toString() {
+		return "Trabajo [idTrabajo=" + idTrabajo + ", descripcion=" + descripcion + ", minSalario=" + minSalario
+				+ ", maxSalario=" + maxSalario + "]";
+	}
+}
